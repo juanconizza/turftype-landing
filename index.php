ed01b82ff9ac57531e9bc239d9b5b9ad7583581e
juanconizza <juanconizza@gmail.com>
2024-09-11 16:35:01 -0300

    fix

--- index.php
+++ index.php
@@ -68,16 +68,17 @@ $_SESSION['num2'] = rand(1, 10);
 
   <body>
     <!-- ======= Header ======= -->
-<header id="header" class="fixed-top header-transparent">
-  <div class="container d-flex align-items-center justify-content-between">
+<header id="header">
+  <div class="container d-flex align-items-center justify-content-between ps-4 pe-4">
     <div class="logo">
       <a href="index.php">
         <img src="assets/img/turftype-espacios-verdes.webp" alt="turftype logo" class="img-fluid logo-img"/>
       </a>
       <img
               src="assets/img/iconos-ventajas-turftype.webp"
-              class="img-fluid w-75 beneficios"
+              class="img-fluid beneficios"
               alt="beneficios"
+              style="width: 60%;"
             />  
     </div>
     <div class="logo2 d-flex flex-column align-items-left text-center">
@@ -93,6 +94,12 @@ $_SESSION['num2'] = rand(1, 10);
     <section id="hero" class="d-flex align-items-center justify-content-center">
       <div class="container">
         <div class="row">
+          <img
+              src="assets/img/iconos-ventajas-turftype.webp"
+              class="img-fluid beneficiosMobile mx-auto mb-3"
+              alt="beneficios"
+              style="width: 80%;"
+            />  
           <!-- Imagen -->
           <div
             class="col-lg-6 d-flex flex-column align-items-center align-items-lg-start order-1 order-lg-2 hero-img"
@@ -118,7 +125,8 @@ $_SESSION['num2'] = rand(1, 10);
               </h2>
               <div class="d-flex mb-3">
                 <svg class="mailIcon img-fluid" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11.5947 19H6.2C5.07989 19 4.51984 19 4.09202 18.782C3.71569 18.5903 3.40973 18.2843 3.21799 17.908C3 17.4802 3 16.9201 3 15.8V8.2C3 7.0799 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569 5.40973 4.09202 5.21799C4.51984 5 5.0799 5 6.2 5H17.8C18.9201 5 19.4802 5 19.908 5.21799C20.2843 5.40973 20.5903 5.71569 20.782 6.09202C21 6.51984 21 7.0799 21 8.2V12M20.6067 8.26229L15.5499 11.6335C14.2669 12.4888 13.6254 12.9165 12.932 13.0827C12.3192 13.2295 11.6804 13.2295 11.0677 13.0827C10.3743 12.9165 9.73279 12.4888 8.44975 11.6335L3.14746 8.09863M18 13.5L19.4107 15.5584L21.8042 16.2639L20.2825 18.2416L20.3511 20.7361L18 19.9L15.6489 20.7361L15.7175 18.2416L14.1958 16.2639L16.5893 15.5584L18 13.5Z" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-              <h3>Solicitá Asesoramiento Profesional personalizado e inmediato</h3>
+              <h3 class="d-none d-md-block">Solicitá Asesoramiento Profesional<br>personalizado e inmediato</h3>
+              <h3 class="d-block d-md-none">Solicitá Asesoramiento Profesional personalizado e inmediato</h3>
               </div>
               <a href="#contact" class="btn-link mb-3">Asesoramiento y Cotización</a>             
             </div>
@@ -398,9 +406,9 @@ $_SESSION['num2'] = rand(1, 10);
           </div>
       
           <!-- Formulario y contacto -->
-          <div class="row justify-content-center mt-5" data-aos="fade-up">
+          <div class="row justify-content-center" data-aos="fade-up">
             <!-- Columna con la imagen y los párrafos -->
-            <div class="col-lg-6 d-flex flex-column align-items-center text-center mb-4">
+            <div class="col-lg-6 d-flex flex-column align-items-center text-center my-5 order-1 order-md-0">
               <img
                 src="assets/img/turftype-espacios-verdes.webp"
                 alt="Logo de Turftype espacios verdes"
@@ -410,11 +418,11 @@ $_SESSION['num2'] = rand(1, 10);
               <p>011 5111-6776</p>
               <p class="reduce-space">Los Plátanos 1823, La Lonja,</p>
               <p class="reduce-space">Pilar, Buenos Aires</p>
-              <svg class="mt-3" style="width: 80px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#9cf36e"></path> <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" fill="#9cf36e"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" fill="#9cf36e"></path> </g></svg>
+              <a href="https://www.instagram.com/turftype"><svg class="mt-3" style="width: 80px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#9cf36e"></path> <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" fill="#9cf36e"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" fill="#9cf36e"></path> </g></svg></a>
             </div>
       
             <!-- Columna con el formulario -->
-            <div class="col-lg-6">
+            <div class="col-lg-6 order-0 order-md-1">
               <p>
                 Solicitamos unos pocos datos de contacto y un <span>especialista</span> se comunicará a la mayor brevedad posible.
               </p>
